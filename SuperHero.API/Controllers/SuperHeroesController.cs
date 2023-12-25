84bb00c19b9dee0b0a9b681de36246a8af45f28f
Bassam Rahat <bassamrahat.ibs@gmail.com>
2023-12-25 16:36:07 +0500

    Add project files.

---
+++ SuperHero.API/Controllers/SuperHeroesController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHero.API.Models;
+
+namespace SuperHero.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroesController : ControllerBase
+    {
+        private readonly SuperHeroContext _context;
+
+        public SuperHeroesController(SuperHeroContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/SuperHeroes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.SuperHero>>> GetSuperHeroes()
+        {
+            return await _context.SuperHeroes.ToListAsync();
+        }
+
+        // GET: api/SuperHeroes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Models.SuperHero>> GetSuperHero(int id)
+        {
+            var superHero = await _context.SuperHeroes.FindAsync(id);
+
+            if (superHero == null)
+            {
+                return NotFound();
+            }
+
+            return superHero;
+        }
+
+        // POST: api/SuperHeroes
+        [HttpPost]
+        public async Task<ActionResult<Models.SuperHero>> PostSuperHero(Models.SuperHero superHero)
+        {
+            _context.SuperHeroes.Add(superHero);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSuperHero", new { id = superHero.Id }, superHero);
+        }
+
+        // PUT: api/SuperHeroes/5
+        [HttpPut]
+        public async Task<IActionResult> PutSuperHero(Models.SuperHero superHero)
+        {
+            var existingSuperHero = _context.SuperHeroes.Where(sh=>sh.Id == superHero.Id);
+            if (existingSuperHero is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(superHero).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SuperHeroExists(superHero.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/SuperHeroes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSuperHero(int id)
+        {
+            var superHero = await _context.SuperHeroes.FindAsync(id);
+            if (superHero == null)
+            {
+                return NotFound();
+            }
+
+            _context.SuperHeroes.Remove(superHero);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SuperHeroExists(int id)
+        {
+            return _context.SuperHeroes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SuperHero.API/Models/SuperHeroContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHero.API.Models
+{
+    public class SuperHeroContext : DbContext
+    {
+        public SuperHeroContext(DbContextOptions<SuperHeroContext> options) : base(options)
+        {
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Additional configurations can be set here
+        }
+    }
+}
